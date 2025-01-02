267bd3e43fcdd80d1102ace2e34d3882fe5e4e2d
Hasini <hasinistudies@gmail.com>
2025-01-02 21:58:56 +0530

    small change

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,21 +1,35 @@
-import Sidebar from "../sidebar/SIdebar"
-import "./navbar.scss"
-import {motion} from "framer-motion"
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
 
 export const NavBar = () => {
   return (
     <div className="navbar">
-        {/* side bar */}
-          <Sidebar/>
-        <div className="wrapper">
-            <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration: 0.5}}>Skyuva</motion.span>
-            <div className="social">
-                <a href="#"><img src="/facebook.png" alt="" /></a>
-                <a href="#"><img src="/instagram.png" alt="" /></a>
-                <a href="#"><img src="/youtube.png" alt="" /></a>
-                <a href="#"><img src="/dribbble.png" alt="" /></a>
-            </div>
+      {/* side bar */}
+      <Sidebar />
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Skyuva
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
